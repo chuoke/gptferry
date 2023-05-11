3676db4ea383d25b0b6c3eda34693345cd12f693
chuoke <chuokechungyoung@gmail.com>
2023-05-11 22:29:03 +0800

    delete console

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
 <script lang="ts" setup>
 import { useQuasar } from "quasar";
-import { useStorage } from "@vueuse/core";
 import { useLocales } from "@/composables/locales";
 import { useI18n } from "vue-i18n";
 
@@ -14,7 +13,7 @@ const { locale } = useI18n({ inheriteLocale: true, useScope: "global" });
 
 onMounted(() => {
   if (!settedLocale.value) {
-    const userLocale = $q.lang.getLocale();
+    const userLocale = $q.lang.getLocale() || "";
 
     if (locales.map((item) => item.value).includes(userLocale)) {
       locale.value = userLocale;
@@ -24,11 +23,5 @@ onMounted(() => {
   } else {
     locale.value = settedLocale.value;
   }
-
-  console.log({
-    settedLocale: settedLocale.value,
-    userLocale: $q.lang.getLocale(),
-    locale: locale.value,
-  });
 });
 </script>
