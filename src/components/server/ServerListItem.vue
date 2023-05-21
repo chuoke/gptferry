1453b5a6f0123943decbee6b7a9244f7cc5d0518
chuoke <chuokechungyoung@gmail.com>
2023-05-21 23:15:16 +0800

    feat: server avatar border-radius

--- src/components/server/ServerListItem.vue
+++ src/components/server/ServerListItem.vue
@@ -1,6 +1,6 @@
 <template>
   <q-tab :name="server.key" class="mb-2">
-    <q-avatar>
+    <q-avatar class="transition-all duration-500 ease-in-out">
       <q-img v-if="server.avatar" :src="server.avatar" />
       <span v-else> {{ server.name }} </span>
     </q-avatar>
@@ -17,3 +17,19 @@ defineProps<{
   server: IServer;
 }>();
 </script>
+
+<style lang="scss" scoped>
+.q-tab--inactive {
+  .q-avatar {
+    &:hover {
+      border-radius: 30%;
+    }
+  }
+}
+
+.q-tab--active {
+  & .q-avatar {
+    border-radius: 25%;
+  }
+}
+</style>
