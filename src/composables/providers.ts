a06536ce10805821fc6a64f9512c70985fe0fa89
chuoke <chuokechungyoung@gmail.com>
2023-06-14 23:08:49 +0800

    feat: add new chatgpt model

--- src/composables/providers.ts
+++ src/composables/providers.ts
@@ -29,11 +29,25 @@ const providers: IProvider[] = [
           "More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat. Will be updated with our latest model iteration.",
       },
       {
+        value: "gpt-4-32k",
+        label: "gpt-4-32k",
+        max_token: 32768,
+        description:
+          "Same capabilities as the base gpt-4 mode but with 4x the context length. Will be updated with our latest model iteration.",
+      },
+      {
         value: "gpt-3.5-turbo",
         label: "gpt-3.5-turbo",
         max_token: 4096,
         description:
           "Most capable GPT-3.5 model and optimized for chat at 1/10th the cost of text-davinci-003. Will be updated with our latest model iteration.",
+      },
+      {
+        value: "gpt-3.5-turbo-16k",
+        label: "gpt-3.5-turbo-16k",
+        max_token: 16384,
+        description:
+          "Same capabilities as the standard gpt-3.5-turbo model but with 4 times the context.",
       },
     ],
     api_base_url: "https://api.openai.com",
